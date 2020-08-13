3a9f329c9797148cea439a56feed456f910ed707
Yaroslav Afanasiev <yaroslav.afanassiev@gmail.com>
2020-08-13 15:59:47 +0300

    Updates

---
+++ precache-manifest.1506a9f8f67afb03a2658dc4bdaaca05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90fa1149fb00ce68662ea534352f50ff",
+    "url": "/react-hooks-forecast-app/index.html"
+  },
+  {
+    "revision": "15fd34953eee8fba96d6",
+    "url": "/react-hooks-forecast-app/static/css/2.3d6e4bb6.chunk.css"
+  },
+  {
+    "revision": "2ba50be10b844f15bbf2",
+    "url": "/react-hooks-forecast-app/static/css/main.6fffc60f.chunk.css"
+  },
+  {
+    "revision": "15fd34953eee8fba96d6",
+    "url": "/react-hooks-forecast-app/static/js/2.1c2395e4.chunk.js"
+  },
+  {
+    "revision": "2ba50be10b844f15bbf2",
+    "url": "/react-hooks-forecast-app/static/js/main.b9337875.chunk.js"
+  },
+  {
+    "revision": "22589caaddf8a4ee8d91",
+    "url": "/react-hooks-forecast-app/static/js/runtime-main.be78237b.js"
+  }
+]);
